fbe7517f674a55a4db7546443ff852c883817e08
MegaGeese <rawhite2@memphis.edu>
2021-04-21 12:54:45 -0500

    Updated MangaDex bot
    bot now will stop running if MangaDex updates

--- MangDexBot/main.py
+++ MangDexBot/main.py
@@ -1,15 +1,26 @@
 import tweepy
 import requests
 import urllib.request
+import datetime
 from datetime import date
+from datetime import time
 from bs4 import BeautifulSoup
 import logging
+
+#TODO Make the bot able to post if MangaDex.org comes back even though bot already posted tweet that day
+#TODO Find a better way to check MangaDex's status
+#TODO If MangaDex.org is donw, the bot will error
 
 logging.basicConfig(level = logging.INFO, filename = "output.log")
 
 user = "IsMangaDexKill"
 url = "https://mangadex.org/index.html"
 check_text = "Three days ago (2021-03-17), we correctly identified and reported that a malicious actor had managed to gain access to an admin account through the reuse of a session token found in an old database leak through faulty configuration of session management. Following that event, we moved to identify the vulnerable section of code and worked to patch it up, also clearing session data globally to thwart further attempts at exploitation through the same method." 
+today = datetime.datetime.today()
+isLive = eval(open("isLive.txt",'r').read())
+post_time = datetime.time(12)
+updated = False
+
 auth = tweepy.OAuthHandler("hULcG0lsEwyeN5ho6IejRzG0R", 
     "fDe7BmJ3HKNK2AtAcJMhry8eCIflzumwF663cNBG3oITFSY69Q")
 
@@ -29,20 +40,22 @@ def checkSite():
 
     if(http_resolved):
         soup = BeautifulSoup(response.text, "html.parser")
-        logging.info(date.today().strftime("%d/%m/%Y") + ": Program Run Successfully")
-        if check_text in soup.get_text(): #If true, website still down, if false website should be live.
-            #post tweet saying down
-            return("Today is: " + date.today().strftime("%d/%m/%Y") + ", and MangaDex is still down :(")
-        else:
+        logging.info(today.strftime("%d/%m/%Y %H:%M:%S") + ": Program Run Successfully")
+        if check_text not in soup.get_text(): #If true website should be live. Update isLive.txt so that the program doesn't keep runnning unnessisarily
+            open("isLive.txt", 'w').write("True")
+            updated = True
             return("The MangaDex website has updated!")
-            #post tweet saying live
-        
-output = checkSite()
+    return("Today is: " + today.strftime("%d/%m/%Y") + ", and MangaDex is still down :(")
 
-logging.info(date.today().strftime("%d/%m/%Y") + ": POST: " + output)
+def main():
+    output = checkSite()
 
-if(api.user_timeline(user)[0].created_at.date() != date.today()):
-    logging.info(date.today().strftime("%d/%m/%Y") + ": Posted Tweet")
-    api.update_status(output)
-else:
-    logging.info(date.today().strftime("%d/%m/%Y") + ": Did not post Tweet")
+    if(not isLive and (today.time() > post_time or updated)):
+
+        if(api.user_timeline(user)[0].created_at.date() != date.today()):
+            logging.info(today.strftime("%d/%m/%Y %H:%M:%S") + ": Posted Tweet")
+            #api.update_status(output)
+        else:
+            logging.info(today.strftime("%d/%m/%Y %H:%M:%S") + ": Did not post Tweet")
+
+main()
